f01d5660d8cce160e96f12bd431e5fc0642df454
Philippe Vinchon <p.vinchon@gmail.com>
2018-11-13 20:21:33 +0000

    Solution from fpinscala was wrong

--- chapter-02-getting-started-with-functional-programming-in-scala/src/test/scala/Answers.scala
+++ chapter-02-getting-started-with-functional-programming-in-scala/src/test/scala/Answers.scala
@@ -12,12 +12,12 @@ object Answers {
     loop(n, 0, 1)
   }
 
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @annotation.tailrec
     def go(n: Int): Boolean =
       if (n >= as.length - 1) true
-      else if (gt(as(n), as(n + 1))) false
-      else go(n + 1)
+      else if (ordered(as(n), as(n + 1))) go(n + 1)
+      else false
 
     go(0)
   }
