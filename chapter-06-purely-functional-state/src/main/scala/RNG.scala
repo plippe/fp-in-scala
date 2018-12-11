079b909a760142f677be587fdc6f610ef98b8e08
Philippe Vinchon <p.vinchon@gmail.com>
2018-12-11 20:01:55 +0000

    Lazy val, or object call will raise unimplemented error

--- chapter-06-purely-functional-state/src/main/scala/RNG.scala
+++ chapter-06-purely-functional-state/src/main/scala/RNG.scala
@@ -39,10 +39,10 @@ object RNG {
   def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
     map2(ra, rb)((_, _))
 
-  val randIntDouble: Rand[(Int, Double)] =
+  lazy val randIntDouble: Rand[(Int, Double)] =
     both(int, double)
 
-  val randDoubleInt: Rand[(Double, Int)] =
+  lazy val randDoubleInt: Rand[(Double, Int)] =
     both(double, int)
 
   /** EXERCISE 6.1
